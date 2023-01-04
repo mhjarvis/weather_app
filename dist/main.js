1dda1dd04ef9c9c63c0d4646f81a9593e1804d86
Markus <markus.h.jarvis@gmail.com>
2023-01-04 09:19:40 -0600

    minor changes to mk main.js readable by adjusting webpack.config.js

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,51 @@
-(()=>{function e(e){fetch(`https://api.openweathermap.org/data/2.5/weather?q=${e}&APPID=bb240cca395c22bffedb2a9a6e9daa71`,{mode:"cors"}).then((function(e){return e.json()})).then((function(e){console.log(e.main)}))}e("Huntsville"),document.querySelector(".city-button").addEventListener("click",(function(){let t=document.querySelector(".city-input").value;document.querySelector(".city-input").value="",console.log(t),e(t)}))})();
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+// api key bb240cca395c22bffedb2a9a6e9daa71
+
+getWeatherData('Huntsville');
+
+// Event Listeners
+const cityButton = document.querySelector('.city-button');
+
+
+cityButton.addEventListener('click', function() {
+
+  let city = document.querySelector('.city-input').value;
+  document.querySelector('.city-input').value = '';
+
+  console.log(city);
+
+  getWeatherData(city);
+})
+
+
+// api call
+function getWeatherData(city) {
+
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=bb240cca395c22bffedb2a9a6e9daa71`, {mode: 'cors'}) 
+/*     .then(function(response) {
+      console.log(response.json())
+    }) */
+    .then(function(response) {
+      return response.json()
+    })
+    .then(function(response) {
+      console.log(response.main)
+    })
+}
+
+
+/*
+Write the functions that hit the API. You’re going to want functions that can take a location and return the weather data for that location. For now, just console.log() the information.
+Write the functions that process the JSON data you’re getting from the API and return an object with only the data you require for your app.
+Set up a simple form that will let users input their location and will fetch the weather info (still just console.log() it).
+Display the information on your webpage!
+Add any styling you like!
+Optional: add a ‘loading’ component that displays from the time the form is submitted until the information comes back from the API.
+Push that baby to github and share your solution below!
+*/
+/******/ })()
+;
--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,6 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  mode: 'production'
+  mode: 'development',
+  devtool: false
 };
