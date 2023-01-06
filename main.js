c21b24f7c37046d6b09fa027d8313eee24878d3c
Markus <markus.h.jarvis@gmail.com>
2023-01-06 11:11:29 -0600

    remove sunset/sunrise section due to conversion issues

--- main.js
+++ main.js
@@ -29,51 +29,19 @@ function getWeatherData(city) {
       return response.json()
     })
     .then(function(response) {
-      console.log(response.main)
-      console.log(response);
-      console.log(response.weather)
       document.querySelector('.city-name').innerHTML = `${response.name} Weather Status`;
       document.querySelector('.current-temperature').innerHTML = `${convertToFarenheit(response.main.temp)} &#xb0;F`;
       document.querySelector('.feels-like').innerHTML = `feels like: ${convertToFarenheit(response.main.feels_like)}&#xb0;F`;
       document.querySelector('.cloud-info').innerHTML = response.weather[0].description;
       document.querySelector('.humidity').innerHTML = `humidity: ${response.main.humidity}%`;
-      document.querySelector('.sunrise').innerHTML = `sunrise: ${format_time(response.sys.sunrise, response.timezone)} a.m.`
-      document.querySelector('.sunset').innerHTML = `sunset: ${format_time(response.sys.sunset)} p.m.`
     })
 }
 
 // Kelvin to Farenheit conversion
-
 function convertToFarenheit(temp) {
   temp = (temp - 273.15) * 9;
   temp = (temp / 5) + 32;
   return parseInt(temp);
 }
-
-// Unix UTC conversion-28800
-console.log(format_time(1673020612, -28800));
-console.log(format_time(1673051580, -28800));
-
-function format_time(UNIX_timestamp, shift){
-  UNIX_timestamp = UNIX_timestamp + shift;
-  let a = new Date(UNIX_timestamp * 1000);
- 
-  let hour = a.getHours();
-  let min = a.getMinutes();
-  let time = hour + ':' + min;
-  return time;
-}
-
-
-
-/*
-Write the functions that hit the API. You’re going to want functions that can take a location and return the weather data for that location. For now, just console.log() the information.
-Write the functions that process the JSON data you’re getting from the API and return an object with only the data you require for your app.
-Set up a simple form that will let users input their location and will fetch the weather info (still just console.log() it).
-Display the information on your webpage!
-Add any styling you like!
-Optional: add a ‘loading’ component that displays from the time the form is submitted until the information comes back from the API.
-Push that baby to github and share your solution below!
-*/
 /******/ })()
 ;
